ee0476c682f31e429bc63b2ad14ba67528e78ab4
Farheenunnisa <141659444+Farheenunnisa@users.noreply.github.com>
2023-09-05 15:21:46 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,30 +1,26 @@
 const todoList = () => {
-  all = []
+  all = [];
   const add = (todoItem) => {
     all.push(todoItem)
-  }
+  };
   const markAsComplete = (index) => {
     all[index].completed = true
-  }
+  };
 
   const overdue = () => {
     const today= new Date().toLocaleDateString("en-CA");
    return all.filter((item)=> item.dueDate<today);
-}
+};
 
   const dueToday = () => {
-    {
       const today= new Date().toLocaleDateString("en-CA");
       return all.filter((item)=> item.dueDate === today);
-   }
-  }
+  };
 
   const dueLater = () => {
-    {
       const today= new Date().toLocaleDateString("en-CA");
       return all.filter((item)=> item.dueDate > today);
-   }
-  }
+  };
 
   const toDisplayableList = (list) => {
    return list
